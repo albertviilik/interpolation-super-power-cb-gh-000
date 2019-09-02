d4e96b0bd7844993c3c52f54155c9f5b65811d43
Albert Viilik <albert.viilik@gmail.com>
2019-09-02 21:59:01 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,7 +2,7 @@
 def display_rainbow(colors)
   counter = 0
   while counter < colors.length
-    print "#{colors[counter][0].upcase}: #{colors[counter]}, "
+    print "#{colors[counter][0].upcase}: #{colors[counter]}"
     counter += 1
   end
 end
